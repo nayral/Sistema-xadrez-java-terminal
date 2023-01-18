f98c6eaddbabb93e4c5d94af2a2f4deaddf3f66f
Nayra Alencar <wolfnay6@gmail.com>
2023-01-18 10:57:54 -0300

    Starting to implement Board and Piece 2

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,5 +1,34 @@
 package boardgame;
 
 public class Board {
+	
+	private int row;
+	private int columns;
+	private Pieces[][] pieces;
+	
+	public Board(int row, int columns) {
+		super();
+		this.row = row;
+		this.columns = columns;
+		pieces = new Pieces [row][columns];
+		
+	}
 
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumns() {
+		return columns;
+	}
+
+	public void setColumns(int columns) {
+		this.columns = columns;
+	}
+	
+	
 }
--- src/boardgame/Pieces.java
+++ src/boardgame/Pieces.java
@@ -11,6 +11,7 @@ public class Pieces {
 		
 		this.board = board;
 		
+		
 	}
 
 
